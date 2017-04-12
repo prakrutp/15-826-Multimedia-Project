0addf31acb519f87e2e30b7796baa59e08fdde39
Megha Arora <marora@GHC39.GHC.ANDREW.CMU.EDU>
2017-04-12 19:58:51 -0400

    Added makefile

--- makefile
+++ makefile
@@ -1,57 +1,12 @@
-top: dcube
-FNAME_A= data_A.inp
-FNAME_B= data_B.inp
-
-hw3: data_A data_B
-
-demo:
-	python q2.py
-
-data_A:
-	python q2.py $(FNAME_A)
-
-data_B:
-	python q2.py $(FNAME_B)
-
-
-
-demo:
+make:
 	@echo ""
-	@echo "--- Q2 ---"
-	cd Q2; make -s
-	@echo ""
-	@echo "--- Q3 ---"
-	cd Q3; make -s
-	@echo ""
-	@echo "--- Q4 ---"
-	cd Q4; make -s
-
-hw3: hw3q2 hw3q3 hw3q4
-
-hw3q2:
-	@echo ""
-	@echo "------- HW3 Q2 ------"
-	cd Q2; make -s hw3
-
-hw3q3:
-	@echo ""
-	@echo "------- HW3 Q3 ------"
-	cd Q3; make -s hw3
-
-hw3q4:
-	@echo ""
-	@echo "------- HW3 Q4 ------"
-	cd Q4; make -s hw3
-
-spotless:
-	\rm -f *hw3.tar.gz
-	cd Q2; make spotless
-	cd Q3; make spotless
-	cd Q4; make spotless
-
-clean:
-
-spotless: clean
+	@echo "----- Data Preparation ------"
+	cd src; python data_cleaning.py
+	@echo "----- D-CUBE Run ------"
+	cd src; python dcube_main.py
 	
-dcube.tar: spotless
-	tar cvf dcube.tar src docs output data makefile 
+spotless:
+	cd src; \rm *.pyc
+	
+tar:
+	tar cvf marora-prakrutp-ph2.tar.gz src output data makefile README.md
--- src/constants.py
+++ src/constants.py
@@ -20,13 +20,13 @@ PGPORT = 15754
 # OUTPUT_LOCATION = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/output/darpa_full_time_SD.txt'
 # INPUT_CSV = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/data/darpa_full_time.csv'
 # RAW_INPUT = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/data/darpa.csv'
-OUTPUT_LOCATION_BLOCKS = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/output/darpa_full_date_SC.csv'
-OUTPUT_LOCATION = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/output/darpa_full_date_SC.txt'
-INPUT_CSV = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/data/darpa_full_date.csv'
+OUTPUT_LOCATION_BLOCKS = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/output/darpa_date_AD.csv'
+OUTPUT_LOCATION = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/output/darpa_date_AD.txt'
+INPUT_CSV = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/data/darpa_date.csv'
 RAW_INPUT = '/afs/andrew.cmu.edu/usr13/marora/private/15826/15-826-Multimedia-Project/data/darpa.csv'
 OUTPUT_TABLE_NAME = 'k_dense_blocks'
 BINARIZE = False
-GRANULARITY = "Date"
+GRANULARITY = "date"
 
 #### Input and output parameters
 INPUT_DELIMITER = ','
@@ -36,6 +36,6 @@ NUM_ATTRIBUTES = 3
 ## Number of dense blocks to be output
 NUM_DENSE_BLOCKS = 50
 ## One among 'A', 'G', 'S'
-DENSITY_MEASURE = 'S'
+DENSITY_MEASURE = 'A'
 ## One among 'C', 'D'
-POLICY = 'C'
+POLICY = 'D'
